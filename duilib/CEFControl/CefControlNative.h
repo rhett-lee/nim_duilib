07debbb336c3c2fb553a7ed5b9ff5b90db2e5e20
rhett-lee <rhettli@hotmail.com>
2025-03-01 08:45:23 +0800

    libcef版本升级：升级到版本【cef_binary_133.4.2+g0852ba6+chromium-133.0.6943.127】（梳理代码）

--- duilib/CEFControl/CefControlNative.h
+++ duilib/CEFControl/CefControlNative.h
@@ -17,7 +17,7 @@ class CefControlNative : public CefControlBase
     typedef CefControlBase BaseClass;
 public:
     explicit CefControlNative(ui::Window* pWindow);
-    ~CefControlNative(void);    
+    virtual ~CefControlNative(void) override;
 
     virtual void Init() override;
     virtual void SetPos(ui::UiRect rc) override;
@@ -25,14 +25,15 @@ public:
     virtual void SetVisible(bool bVisible) override;
     virtual void SetWindow(ui::Window* pWindow) override;
 
-    /**
-    * @brief 打开开发者工具
+    /** 打开开发者工具
     * @param[in] view 一个 CefControlOffScreen 控件实例(仅在CefControlOffScreen类里需要传入)
     * @return 成功返回 true，失败返回 false
     */
     virtual bool AttachDevTools(ui::Control* view) override;
 
 protected:
+    /** 重新创建Browser对象
+    */
     virtual void ReCreateBrowser() override;
 };
 }
