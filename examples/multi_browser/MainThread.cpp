1bb03a113f31738e2ba4e1db6d499d358cb22784
rhett-lee <rhettli@hotmail.com>
2025-03-02 21:24:47 +0800

    libcef源码升级到cef_binary_133.4.5+gdb28106+chromium-133.0.6943.142版（Linux编译的兼容）

---
+++ examples/multi_browser/MainThread.cpp
@@ -0,0 +1,35 @@
+#include "MainThread.h"
+#include "browser/multi_browser_manager.h"
+
+#include <chrono>
+
+MainThread::MainThread() :
+    FrameworkThread(_T("MainThread"), ui::kThreadUI)
+{
+}
+
+MainThread::~MainThread()
+{
+}
+
+const ui::DpiInitParam& MainThread::GetDpiInitParam() const
+{
+    return m_dpiInitParam;
+}
+
+void MainThread::OnInit()
+{   
+    //初始化全局资源, 使用本地文件夹作为资源
+    ui::FilePath resourcePath = ui::FilePathUtil::GetCurrentModuleDirectory();
+    resourcePath += _T("resources\\");
+    ui::GlobalManager::Instance().Startup(ui::LocalFilesResParam(resourcePath), GetDpiInitParam());
+
+    uint64_t nTimeMS = std::chrono::steady_clock::now().time_since_epoch().count() / 1000;
+    std::string timeStamp = ui::StringUtil::Printf("%I64u", nTimeMS);
+    MultiBrowserManager::GetInstance()->CreateBorwserBox(nullptr, timeStamp, _T(""));
+}
+
+void MainThread::OnCleanup()
+{
+    ui::GlobalManager::Instance().Shutdown();
+}
