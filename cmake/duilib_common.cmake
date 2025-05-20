96a9617f988c9b6a7f0b0bc3b593749e78720b1d
rhett-lee <rhettli@hotmail.com>
2025-05-20 20:11:43 +0800

    兼容MinGW-w64环境下的编译的相关修改（Do you support the MinGW-w64 compiler? #136）：除了cef,CefBrowser程序，其他程序都编译通过了。 libCEF目前还不支持MinGW-w64的编译，有编译错误暂未能修复。

---
+++ cmake/duilib_common.cmake
@@ -0,0 +1,180 @@
+# 操作系统：Windows,Linux,MacOS
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    set(DUILIB_OS_WINDOWS 1)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(DUILIB_OS_LINUX 1)
+elseif(APPLE)
+    set(DUILIB_OS_MACOS 1)
+else()
+    message(WARNING "Unknown OS: ${CMAKE_SYSTEM_NAME}")
+endif()
+
+# MINGW 开发环境
+if(MINGW)
+    set(DUILIB_MINGW 1)
+    set(DUILIB_MINGW_NAME "mingw64-")
+    
+    # MingGW-w64编译时，使用静态链接
+    option(DUILIB_MINGW_STATIC "MINGW Static Link" ON)
+    
+endif()
+
+# 定义开关变量（修改后，需要清除camke生成目录才能生效，如果未清除则会从缓存中读取旧值）
+option(DULIB_LOG "Print duilib debug log" OFF)
+
+# SDL功能：Windows平台默认不开启，其他平台默认开启
+if(DUILIB_OS_WINDOWS)
+    option(DUILIB_ENABLE_SDL "Enable SDL" OFF)
+else()
+    option(DUILIB_ENABLE_SDL "Enable SDL" ON)
+endif()
+
+# CEF功能：默认不开启，只有特定的项目开启
+option(DUILIB_ENABLE_CEF "Enable CEF" OFF)
+
+# CEF 109版本是否开启，默认不开启（CEF109版本兼容Win7系统，其他CEF版本只能运行在Win10及高版本操作系统）
+option(DUILIB_CEF_109 "Enable CEF 109" OFF)
+
+#编译器类型: msvc,gcc,llvm
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    set(DUILIB_COMPILER_MSVC 1)
+    set(DUILIB_COMPILER_NAME "msvc")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(DUILIB_COMPILER_GCC 1)
+    set(DUILIB_COMPILER_NAME "${DUILIB_MINGW_NAME}gcc")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(DUILIB_COMPILER_LLVM 1)
+    set(DUILIB_COMPILER_NAME "${DUILIB_MINGW_NAME}llvm")
+else()
+    set(DUILIB_COMPILER_NAME "unknown")
+    message(WARNING "Unknown CMAKE_CXX_COMPILER_ID: ${CMAKE_CXX_COMPILER_ID}")
+endif() 
+
+#CPU类型
+string(TOLOWER "${CMAKE_SYSTEM_PROCESSOR}" lower_processor)
+if(lower_processor MATCHES "armv7|arm")
+    set(DUILIB_SYSTEM_PROCESSOR "arm32")
+elseif(lower_processor MATCHES "aarch64|arm64")
+    set(DUILIB_SYSTEM_PROCESSOR "arm64")
+elseif(lower_processor MATCHES "i[3-6]86")
+    set(DUILIB_SYSTEM_PROCESSOR "x86")
+elseif(lower_processor MATCHES "x86_64|amd64")
+    set(DUILIB_SYSTEM_PROCESSOR "x64")
+else()
+    set(DUILIB_SYSTEM_PROCESSOR, "unknown")
+    message(WARNING "Unknown CMAKE_SYSTEM_PROCESSOR: ${CMAKE_SYSTEM_PROCESSOR}")
+endif()
+
+#64位还是32位
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(DUILIB_BITS_64 1)
+endif()
+
+#编译类型：Debug还是Release
+set(DUILIB_BUILD_TYPE "release")
+
+#dulib源码根目录，lib文件目录，bin文件目录
+get_filename_component(DUILIB_SRC_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../" ABSOLUTE)
+set(DUILIB_LIB_PATH "${DUILIB_SRC_ROOT_DIR}/libs")
+set(DUILIB_BIN_PATH "${DUILIB_SRC_ROOT_DIR}/bin")
+set(DUILIB_LIBS duilib duilib-cximage duilib-webp duilib-png duilib-zlib)
+
+#CEF模块的源码根目录
+if(DUILIB_OS_WINDOWS AND DUILIB_ENABLE_CEF)
+    if (DUILIB_CEF_109)
+        #使用CEF 109版本
+        set(CEF_SRC_ROOT_DIR "${DUILIB_SRC_ROOT_DIR}/duilib/third_party/libcef_win")
+        set(CEF_WRAPPER_LIB_NAME cef_dll_wrapper_109)
+    else()
+        #使用CEF最新版本
+        set(CEF_SRC_ROOT_DIR "${DUILIB_SRC_ROOT_DIR}/duilib/third_party/libcef_win")
+        set(CEF_WRAPPER_LIB_NAME cef_dll_wrapper)
+    endif()
+elseif(DUILIB_OS_LINUX)
+    set(CEF_SRC_ROOT_DIR "${DUILIB_SRC_ROOT_DIR}/duilib/third_party/libcef_linux")
+    set(CEF_WRAPPER_LIB_NAME cef_dll_wrapper)
+elseif(DUILIB_OS_MACOS)
+    set(CEF_SRC_ROOT_DIR "${DUILIB_SRC_ROOT_DIR}/duilib/third_party/libcef_macos")
+    set(CEF_WRAPPER_LIB_NAME cef_dll_wrapper)
+endif()
+
+#Skia源码根目录，lib文件目录
+get_filename_component(SKIA_SRC_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../skia/" ABSOLUTE)
+set(SKIA_LIB_PATH "${SKIA_SRC_ROOT_DIR}/out/${DUILIB_COMPILER_NAME}.${DUILIB_SYSTEM_PROCESSOR}.${DUILIB_BUILD_TYPE}")
+set(SKIA_LIBS svg skia skshaper)
+
+#SDL库根目录
+if(DUILIB_ENABLE_SDL)
+    get_filename_component(SDL_SRC_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../SDL3/" ABSOLUTE)
+    if(EXISTS "${SDL_SRC_ROOT_DIR}/lib64/")
+        set(SDL_LIB_PATH "${SDL_SRC_ROOT_DIR}/lib64")
+    else()
+        set(SDL_LIB_PATH "${SDL_SRC_ROOT_DIR}/lib")
+    endif()
+    set(SDL_LIBS SDL3)
+endif()
+
+if(DULIB_LOG)
+    message(STATUS "DUILIB_PROJECT_SRC_DIR: ${DUILIB_PROJECT_SRC_DIR}")
+    message(STATUS "PROJECT_NAME: ${PROJECT_NAME}")
+    if(DUILIB_OS_WINDOWS)
+        message(STATUS "DUILIB_WINRES_FILE_NAME: ${DUILIB_WINRES_FILE_NAME}")
+    endif()
+    message(STATUS "") 
+
+    if(DUILIB_OS_WINDOWS)
+        message(STATUS "DUILIB_OS: Windows")
+    endif()
+    if(DUILIB_OS_LINUX)
+        message(STATUS "DUILIB_OS: Linux")
+    endif()
+    if(DUILIB_OS_MACOS)
+        message(STATUS "DUILIB_OS: MacOS")
+    endif()
+
+    message(STATUS "DUILIB_COMPILER_NAME: ${DUILIB_COMPILER_NAME}")
+    message(STATUS "DUILIB_SYSTEM_PROCESSOR: ${DUILIB_SYSTEM_PROCESSOR}")
+
+    if(DUILIB_MINGW)
+        message(STATUS "DUILIB_MINGW: ON")
+        message(STATUS "DUILIB_MINGW_STATIC: ${DUILIB_MINGW_STATIC}")
+    else()
+        message(STATUS "DUILIB_MINGW: OFF")
+    endif()
+
+    if(DUILIB_BITS_64)
+        message(STATUS "DUILIB_BITS_64: ON")
+    else()
+        message(STATUS "DUILIB_BITS_64: OFF")
+    endif()
+
+    message(STATUS "DUILIB_BUILD_TYPE: ${DUILIB_BUILD_TYPE}")
+    message(STATUS "") 
+    
+    message(STATUS "DUILIB_SRC_ROOT_DIR: ${DUILIB_SRC_ROOT_DIR}")
+    message(STATUS "DUILIB_BIN_PATH: ${DUILIB_BIN_PATH}")
+    message(STATUS "DUILIB_LIB_PATH: ${DUILIB_LIB_PATH}")    
+    message(STATUS "DUILIB_LIBS: ${DUILIB_LIBS}") 
+    message(STATUS "") 
+    
+    message(STATUS "SKIA_SRC_ROOT_DIR: ${SKIA_SRC_ROOT_DIR}")
+    message(STATUS "SKIA_LIB_PATH: ${SKIA_LIB_PATH}")
+    message(STATUS "SKIA_LIBS: ${SKIA_LIBS}")
+    message(STATUS "") 
+    
+    message(STATUS "DUILIB_ENABLE_CEF: ${DUILIB_ENABLE_CEF}") 
+    if (DUILIB_ENABLE_CEF)        
+        message(STATUS "CEF_SRC_ROOT_DIR: ${CEF_SRC_ROOT_DIR}") 
+        message(STATUS "DUILIB_CEF_109: ${DUILIB_CEF_109}") 
+        message(STATUS "CEF_WRAPPER_LIB_NAME: ${CEF_WRAPPER_LIB_NAME}")
+    endif()
+    message(STATUS "") 
+    
+    message(STATUS "DUILIB_ENABLE_SDL: ${DUILIB_ENABLE_SDL}")
+    if(DUILIB_ENABLE_SDL)        
+        message(STATUS "SDL_SRC_ROOT_DIR: ${SDL_SRC_ROOT_DIR}")
+        message(STATUS "SDL_LIB_PATH: ${SDL_LIB_PATH}")
+        message(STATUS "SDL_LIBS: ${SDL_LIBS}")
+    endif()
+    message(STATUS "") 
+endif()
