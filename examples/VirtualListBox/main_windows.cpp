27a298e9aeb03f2b1f0df4daaf1b24b67e669c07
rhett-lee <rhettli@hotmail.com>
2025-05-20 13:31:07 +0800

    兼容MinGW-w64环境下的编译的相关修改（Do you support the MinGW-w64 compiler? #136）： 应用程序的入口函数兼容MinGW-w64（使用WinMain作为入口函数，而不是wWinMain）

--- examples/VirtualListBox/main_windows.cpp
+++ examples/VirtualListBox/main_windows.cpp
@@ -4,10 +4,18 @@
 #include "TestApplication.h"
 
 //定义应用程序的入口点
+#if defined (__MINGW32__) || (__MINGW64__)
+//使用MinGW-w64编译
+int APIENTRY WinMain(_In_ HINSTANCE /*hInstance*/,
+                     _In_opt_ HINSTANCE /*hPrevInstance*/,
+                     _In_ LPSTR    /*lpCmdLine*/,
+                     _In_ int       /*nCmdShow*/)
+#else
 int APIENTRY wWinMain(_In_ HINSTANCE /*hInstance*/,
                       _In_opt_ HINSTANCE /*hPrevInstance*/,
                       _In_ LPWSTR    /*lpCmdLine*/,
                       _In_ int       /*nCmdShow*/)
+#endif
 {
     TestApplication app;
     app.Run();
