25dd3a1dbe61778aebb51cce4eb4dbdd549e1225
rhett-lee <rhettli@hotmail.com>
2025-05-21 14:12:00 +0800

    修改编译脚本：支持SKia子目录固定为某种编译器的子目录（比如Linux平台，固定用llvm）

--- build/linux_build.sh
+++ build/linux_build.sh
@@ -2,12 +2,18 @@
 
 DUILIB_SRC_ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 echo "DUILIB_SRC_ROOT_DIR: $DUILIB_SRC_ROOT_DIR"
- 
 
-# 设置编译器为gcc/g++
-DUILIB_CMAKE="cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
+# 设置编译器
+SET DUILIB_CC=gcc
+SET DUILIB_CXX=g++
+SET DUILIB_COMPILER_ID=gcc
+
+DUILIB_CMAKE="cmake -DCMAKE_C_COMPILER=$DUILIB_CC -DCMAKE_CXX_COMPILER=$DUILIB_CXX"
 DUILIB_MAKE="cmake --build"
 DUILIB_MAKE_THREADS="-j 6"
+
+# Skia库的子目录，固定使用LLVM编译的，如果想使用默认规则，可注释掉这行
+DUILIB_SKIA_LIB_SUBPATH=llvm.x64.release
 
 # lib目录
 target_dir="$DUILIB_SRC_ROOT_DIR/libs/"
@@ -16,7 +22,7 @@ if [[ ! -d "$target_dir" ]]; then
 fi
 
 # 编译临时目录
-DUILIB_BUILD_DIR="$DUILIB_SRC_ROOT_DIR/build_temp/gcc-mingw-w64"
+DUILIB_BUILD_DIR="$DUILIB_SRC_ROOT_DIR/build_temp/$DUILIB_COMPILER_ID_build"
 
 target_dir="$DUILIB_BUILD_DIR"
 if [[ ! -d "$target_dir" ]]; then
@@ -37,6 +43,6 @@ $DUILIB_MAKE "$DUILIB_BUILD_DIR/duilib" $DUILIB_MAKE_THREADS
 #编译examples下的各个程序
 DUILIB_PROGRAMS=("basic" "controls" "ColorPicker" "DpiAware" "layouts" "ListBox" "ListCtrl" "MoveControl" "MultiLang" "render" "RichEdit" "VirtualListBox" "threads" "TreeView" "cef" "CefBrowser")
 for duilib_bin in "${DUILIB_PROGRAMS[@]}"; do
-    $DUILIB_CMAKE -S "$DUILIB_SRC_ROOT_DIR/examples/$duilib_bin" -B "$DUILIB_BUILD_DIR/$duilib_bin" -DCMAKE_BUILD_TYPE=Release
+    $DUILIB_CMAKE -S "$DUILIB_SRC_ROOT_DIR/examples/$duilib_bin" -B "$DUILIB_BUILD_DIR/$duilib_bin" -DCMAKE_BUILD_TYPE=Release -DDUILIB_SKIA_LIB_SUBPATH="$DUILIB_SKIA_LIB_SUBPATH"
     $DUILIB_MAKE "$DUILIB_BUILD_DIR/$duilib_bin" $DUILIB_MAKE_THREADS
 done
