ee69647d9f2dbb4421d05966b193d2fdad784c93
rhett-lee <rhettli@hotmail.com>
2025-06-06 20:21:55 +0800

    controls示例程序：规范代码，并增加一个测试页面，用来测试控件。

---
+++ examples/controls/TestForm.cpp
@@ -0,0 +1,26 @@
+#include "TestForm.h"
+
+TestForm::TestForm()
+{
+}
+
+TestForm::~TestForm()
+{
+}
+
+DString TestForm::GetSkinFolder()
+{
+    return _T("controls");
+}
+
+DString TestForm::GetSkinFile()
+{
+    return _T("test.xml");
+}
+
+void TestForm::OnInitWindow()
+{
+
+
+}
+
