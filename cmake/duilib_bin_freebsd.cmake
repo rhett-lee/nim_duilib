be4939c7615f3ac9b66a1ed0ebef229ed4fda7fa
rhett-lee <rhettli@hotmail.com>
2025-11-07 10:01:24 +0800

    cmake/duilib_bin_freebsd.cmake: Skia相关的配置使用公共定义的配置，不需要个性化定制。

--- cmake/duilib_bin_freebsd.cmake
+++ cmake/duilib_bin_freebsd.cmake
@@ -12,19 +12,6 @@ add_executable(${PROJECT_NAME} ${SRC_FILES})
 # 平台的标准库
 set(DUILIB_FREEBSD_LIBS pthread dl)
 
-#Skia源码根目录，lib文件目录（Skia必选项）
-get_filename_component(DUILIB_SKIA_SRC_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../skia/" ABSOLUTE)
-if(DUILIB_SKIA_LIB_SUBPATH STREQUAL "" OR DUILIB_SKIA_LIB_SUBPATH STREQUAL "OFF")
-    # 根据规则拼接
-    set(DUILIB_SKIA_LIB_PATH "${DUILIB_SKIA_SRC_ROOT_DIR}/out/${DUILIB_COMPILER_NAME}.${DUILIB_SYSTEM_PROCESSOR}.${DUILIB_BUILD_TYPE}")
-    message(STATUS "DUILIB_SKIA_LIB_PATH: ${DUILIB_SKIA_LIB_PATH}")
-    message(STATUS "DUILIB_SKIA_SRC_ROOT_DIR: ${DUILIB_SKIA_SRC_ROOT_DIR}")
-else()
-    # 外部指定的子目录名
-    set(DUILIB_SKIA_LIB_PATH "${DUILIB_SKIA_SRC_ROOT_DIR}/out/${DUILIB_SKIA_LIB_SUBPATH}")
-endif()
-set(DUILIB_SKIA_LIBS libsvg.a libskia.a libskshaper.a)
-
 find_package(Freetype REQUIRED)
 find_package(Fontconfig REQUIRED)
 find_package(X11 REQUIRED)
