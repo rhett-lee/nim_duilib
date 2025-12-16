a565d9d12bc0472da534552bf89cf25bc912dd6d
rhett-lee <rhettli@hotmail.com>
2025-12-16 22:14:38 +0800

    VirtualListBox：修复虚表中ElementIndex与ItemIndex映射关系中的潜在错误。

--- duilib/Layout/VirtualHLayout.cpp
+++ duilib/Layout/VirtualHLayout.cpp
@@ -268,6 +268,11 @@ void VirtualHLayout::LazyArrangeChild(UiRect rc) const
             if (pControl->IsVisible()) {
                 pControl->SetVisible(false);
             }
+            //需要清除ElementIndex
+            IListBoxItem* pListBoxItem = dynamic_cast<IListBoxItem*>(pControl);
+            if (pListBoxItem != nullptr) {
+                pListBoxItem->SetElementIndex(Box::InvalidIndex);
+            }
         }
         ++iCount;
 
